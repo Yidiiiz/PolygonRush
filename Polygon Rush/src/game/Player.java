135d530b1ca3d6b726a9394b9cf32b2d3db7e383
Flameyzyzyz <876yidi@gmail.com>
2025-03-08 17:32:21 -0500

    Comments
    Added comments for clarity

--- Polygon Rush/src/game/Player.java
+++ Polygon Rush/src/game/Player.java
@@ -1,67 +1,88 @@
 package game;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
-
+// Player class, creates and moves the player
 public class Player extends Polygon {
-	public double yVel = 0;
-	public double xVel = 0;
-	
-	public boolean isAlive = true;
-	
+	// Changeable gravity and jump power
 	private static double gravity = -1;
 	private static double jumpPower = 15;
 	
+	// Initial velocity
+	public double yVel = 0;
+	public double xVel = 0;
+	
+	// Is player alive
+	public boolean isAlive = true;
+	
+	// Can player currently jump
 	private boolean canJump = true;
+	
+	// Last polygon collided (used to check new collisions)
 	public Polygon newCollide;
  	
+	// Constructor for player object
 	public Player(Point[] inShape, Point inPosition, int rotation) {
 		super(inShape, inPosition, rotation);
 	}
 	
+	// When jump keys pressed, this function is triggered, toggling the player jump     
 	public void jump(Polygon floor) {
-		if (canJump) {// this.collides(floor)
+		if (canJump) {
 			yVel = jumpPower;
 			canJump = false;
 		}
 	}
 	
-	public void move(Polygon floor, Map m) {		
+	// Move function for player, including collision and jump logic
+	public void move(Polygon floor, Map m, int mapSpeed) {		
+		// Moves the player based on current player velocity
 		super.position.y -= yVel;
 		super.position.x += xVel;
-	
 		
-		if (super.collides(floor) || collidesMap(m)) { // (super.position.y >= 400) {
-			
-			
-			if (super.collides(floor)) {
-				super.position.y = floor.position.y - 30 - yVel;
-				newCollide = floor;
-			} else {
-				for (MapElement e : m.mapArray) {
-					if (super.collides((Polygon) e)) {
-						if (this.position.y + yVel <= ((Polygon) e).position.y - 30 + gravity) {
-							super.position.y = ((Polygon) e).position.y - 30 - yVel;
-							break;
-						} else if (newCollide != (Polygon) e && this.position.x + 30 < ((Polygon) e).position.x + 3) {
-							newCollide = (Polygon) e;
-							System.out.println("die");
-							isAlive = false;
-						}
+		// Gets the map element which the player collides with, null if none
+		MapElement e = collidesMap(m);
+		
+		// If player collides with floor or map
+		if (super.collides(floor) || e != null) {
+			// If player collides with an element on the map
+			if (e != null) {
+				// If element is collided
+				if (super.collides((Polygon) e)) {
+					// If player fell from above the element, player is placed on top of it
+					if (this.position.y + yVel <= ((Polygon) e).position.y - 30 + gravity) {
+						super.position.y = ((Polygon) e).position.y - 30 - yVel;
+						break;
+						
+					// Otherwise, if the player is colliding the block from the side, player dies
+					} else if (newCollide != (Polygon) e && this.position.x + 30 < ((Polygon) e).position.x + 3) {
+						newCollide = (Polygon) e;
+						isAlive = false;
 					}
 				}
+			// If player is colliding the floor
+			} else {
+				super.position.y = floor.position.y - 30 - yVel;
+				newCollide = floor;
 			}
 			
+			// Since player is inside of something, movement is undone
 			super.position.y += yVel;
 			super.position.x -= xVel;
 			
+			// Resets velocity because of collision
 			yVel = 0;
+			
+			// Allows player to jump again due to them touching ground
 			canJump = true;
+			
+			// Resets player rotation
 			super.rotation = 0;
+			
+		// If nothing collided
 		} else {
+			// Increments gravity on velocity
 			yVel += gravity;
 			
+			// If player is currently jumping, player rotates at a speed relative to jump power and gravity
 			if (!canJump) {
 				super.rotation += 270/Math.abs((jumpPower)/gravity)/3;
 			}
@@ -69,12 +90,17 @@ public class Player extends Polygon {
 		
 	}
 	
-	public boolean collidesMap(Map m) {
+	// Gets the map element which the player collides with, null if none
+	public MapElement collidesMap(Map m) {
+		// Checks through each element of the map
 		for (MapElement e : m.mapArray) {
+			// If player collides with element, return the element
 			if (super.collides((Polygon) e)) {
-				return true;
+				return e;
 			}
 		}
-		return false;
+		
+		// returns null if player does not collide with any map elements
+		return null;
 	}
 }
--- Polygon Rush/src/game/PolygonRush.java
+++ Polygon Rush/src/game/PolygonRush.java
@@ -1,133 +1,132 @@
 package game;
-
-/*
-CLASS: YourGameNameoids
-DESCRIPTION: Extending Game, YourGameName is all in the paint method.
-NOTE: This class is the metaphorical "main method" of your program,
-      it is your control center.
-
-*/
 import java.awt.*;
 import java.awt.event.*;
 
+
+// Main class, runs the game
 class PolygonRush extends Game implements KeyListener {
+	// Speed which map scrolls
+	private static int mapSpeed = 3;
+	
+	// Time counter (given at start)
 	static int counter = 0;
+	
+	// Variables for player, floor, and map
 	public Player player;
 	public Polygon floor;
 	public Map m;
 	
-
+	// Constructor for game, creates the game and initializes objects
 	public PolygonRush() {
+		// Creating the game
 		super("YourGameName!",1200,600);
 	    this.setFocusable(true);
 	    this.requestFocus();
+	    
+	    // Creates a key listener to detect key presses
 	    addKeyListener(this);
 
+	    // Creates and initializes the player
 	    Point[] playerShape = new Point[] {new Point(0, 0), new Point(0, 30), new Point(30, 30), new Point(30, 0)};
 	    Point playerPosition = new Point(100, height - 100 - 30);
    		player = new Player(playerShape, playerPosition, 0);
 
+   		// Creates and initializes the floor
    		Point[] floorShape = new Point[] {new Point(0, 0), new Point(0, 100), new Point(width, 100), new Point(width, 0)};
    		Point floorPosition = new Point(0, height - 100);
 	    floor = new Polygon(floorShape, floorPosition, 0);
-	    
-//	    Point[] newShape = new Point[] {new Point(0, 0), new Point(0, 30), new Point(30, 30), new Point(30, 0)};
-//	    Point newPosition = new Point(width, height - 100 - 30);
-//   		Polygon p = new Polygon(newShape, newPosition, 0);
    		
+	    // Creates and initializes the map
    		m = new Map();
-   		m.addElement(new Platform(30, new Point(200, height - 100 - 30), 0, Color.black));
+   		m.addElement(new Platform(30, new Point(width, height - 100 - 30), 0, Color.black));
 	}
 
-  
+	// Paint method, runs each frame and draws all objects and UI
 	public void paint(Graphics brush) {
+		// Creating the background of the game
     	brush.setColor(Color.white);
     	brush.fillRect(0,0,width,height);
     	
+    	// Set brush color to black
     	brush.setColor(Color.black);
-    	
+    	    	
     	// Draw floor
-    	Point[] floorPoints = floor.getPoints();
-    	int[] floorXPoints = new int[floorPoints.length];
-    	int[] floorYPoints = new int[floorPoints.length];
-    	
-    	for (int i = 0; i < floorPoints.length; i++) {
-    		floorXPoints[i] = (int) Math.floor(floorPoints[i].getX());
-    		floorYPoints[i] = (int) Math.floor(floorPoints[i].getY());
-    	}
-    	
-    	brush.fillPolygon(floorXPoints, floorYPoints, floorPoints.length);
-    	
+    	int[][] xy = getXY(floor.getPoints());
+    	brush.fillPolygon(xy[0], xy[1], xy[0].length);
     	
     	// Draw player
-    	Point[] playerPoints = player.getPoints();
-    	int[] xPoints = new int[playerPoints.length];
-    	int[] yPoints = new int[playerPoints.length];
-    	 
-    	for (int i = 0; i < playerPoints.length; i++) {
-    		xPoints[i] = (int) playerPoints[i].getX();
-       		yPoints[i] = (int) playerPoints[i].getY();
-    	}
-    	
-    	brush.fillPolygon(xPoints, yPoints, playerPoints.length);
-    	player.move(floor, m);
+    	xy = getXY(player.getPoints());
+    	brush.fillPolygon(xy[0], xy[1], xy[0].length);
+    	player.move(floor, m, mapSpeed);
 
+    	// On player death, reset player and map
     	if (!player.isAlive) {
     		player.isAlive = true;
     		player.newCollide = null;
     		for (MapElement e : m.mapArray) {
         		Polygon p = (Polygon) e;
-        		p.position.x = 200;
+        		p.position.x = width;
         	}
     	}
 
+    	// Draws all map elements
     	for (MapElement e : m.mapArray) {
     		Polygon p = (Polygon) e;
-    		p.position.x -= 3;
+    		p.position.x -= mapSpeed;
+    		
+    		// If map element is out of view to the left, it respawn's on the right (probably will not keep)
     		if (p.position.x <= -30) {
-    			p.position.x = 200;
+    			p.position.x = width;
     		}
-    		playerPoints = p.getPoints();
-        	xPoints = new int[playerPoints.length];
-        	yPoints = new int[playerPoints.length];
-        	 
-        	for (int i = 0; i < playerPoints.length; i++) {
-        		xPoints[i] = (int) playerPoints[i].getX();
-           		yPoints[i] = (int) playerPoints[i].getY();
-        	}
-        	
-        	brush.fillPolygon(xPoints, yPoints, playerPoints.length);
+    		
+    		// Draws current map element
+        	xy = getXY(p.getPoints());
+        	brush.fillPolygon(xy[0], xy[1], xy[0].length);
     	}
     	
+    	//  GIVEN CODE, WAS ALWAYS HERE (probably won't keep)
     	// sample code for printing message for debugging
     	// counter is incremented and this message printed
     	// each time the canvas is repainted
-    	
     	counter++;
     	brush.setColor(Color.black);
     	brush.drawString("Counter is " + counter,10,10);
 	}
-  
+	
+	// Function to get both x and y arrays in an array when drawing an element
+	public int[][] getXY(Point[] p) {
+		int[] xPoints = new int[p.length];
+		int[] yPoints = new int[p.length];
+
+    	for (int i = 0; i < p.length; i++) {
+    		xPoints[i] = (int) p[i].getX();
+    		yPoints[i] = (int) p[i].getY();
+    	}
+    	
+    	int[][] returnMe = new int[][] {xPoints, yPoints};
+		return returnMe;
+	}
+
+	// Main method, creates and starts the game
 	public static void main (String[] args) {
    		PolygonRush a = new PolygonRush();
 		a.repaint();
 	}
 
-
+	// On key pressed event (required by keyListener)
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyChar() == KeyEvent.VK_SPACE || e.getKeyChar() == KeyEvent.VK_W || e.getKeyChar() == KeyEvent.VK_UP) {
 			player.jump(floor);
 		}
 	}
 	
+	// On key typed event (required by keyListener)
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
-
+	// On key released event (required by keyListener)
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 }
